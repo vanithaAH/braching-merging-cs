aee36bcf53ed5fca67f5449bd50c032636f46687
vanithaAH <vanithash123@gmail.com>
2022-07-20 18:15:03 +0530

    Added HiCode.cs file

---
+++ HiCode.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CSharpSolutions
+{
+    class HiCode
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hi World");
+
+        }
+    }
+}
